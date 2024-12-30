9ad25ba2c277e1782e01aa369358752a6ba8b190
Laxmunaidu <luckyyenduva8.7@gmail.com>
2024-12-30 22:41:35 +0530

    password visibility set

--- frontend/probo/src/components/Login.jsx
+++ frontend/probo/src/components/Login.jsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import { Input } from './ui/input'
 import Button from './Button'
 import { Link,useNavigate } from 'react-router-dom'
+import { Eye, EyeOff } from 'lucide-react';
+
 
 
 const Login = () => {
@@ -9,6 +11,11 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const [passwordVisible, setPasswordVisisble] = useState(false)
+    
+    const togglePasswordVisible = () => {
+        setPasswordVisisble((prevState) => !prevState)
+    }
 
     const handleUsernameChange = (e) => {
         setUsername(e.target.value)
@@ -46,7 +53,16 @@ const Login = () => {
         <div className='grid justify-center p-10 shadow-md rounded-xl bg-slate-50 h-[390px] mt-16'>
             <h1 className='font-semibold text-2xl'>Login</h1>
             <Input type="text" placeholder="Username" required={true} value={username} onChange={handleUsernameChange} className="w-72 h-10" />
-            <Input type="password" placeholder="Password" required={true} value={password} onChange={handlePasswordChange} className="w-72 h-10" />
+            <div className='flex gap-3'>
+                <Input type={passwordVisible ? "text": "password"} placeholder="Password" name="password" required={true} value={password} onChange={handlePasswordChange} className="w-64 h-10" />
+                {
+                    passwordVisible ? (
+                        <EyeOff className='mt-2 cursor-pointer' onClick={togglePasswordVisible} />
+                    ):(
+                        <Eye className='mt-2 cursor-pointer' onClick={togglePasswordVisible} />
+                    )
+                }
+            </div>            
             <h2>Dont't have an account? <Link to='/signup'><span className='text-red-900 font-semibold'>Signup</span></Link></h2>
             <Button text='Login' variant='secondary' onClick={handleLogin} />
         </div>
--- frontend/probo/src/components/Signup.jsx
+++ frontend/probo/src/components/Signup.jsx
@@ -2,11 +2,19 @@ import React, { useState } from 'react'
 import { Input } from './ui/input'
 import Button from './Button'
 import { Link,useNavigate } from 'react-router-dom'
+import { Eye, EyeOff } from 'lucide-react';
+
+
 
 const Signup = () => {
     const [error,setError] = useState("");
     const [username,setUsername] = useState("")
     const [password,setPassword] = useState("")
+    const [passwordVisible, setPasswordVisisble] = useState(false)
+
+    const togglePasswordVisible = () => {
+        setPasswordVisisble((prevState) => !prevState)
+    }
 
     const navigate = useNavigate();
 
@@ -38,13 +46,28 @@ const Signup = () => {
         }
     } 
   return (
-    <div className='p-5 flex justify-center h-screen'>
+    <div className='p-5 grid justify-center h-screen'>
         <div className='grid justify-center p-10 shadow-md rounded-xl bg-slate-50 h-[390px] mt-16'>
             <h1 className='font-semibold text-2xl'>Signup</h1>
             <Input type="text" placeholder="Username"  name='username' required={true} value={username} onChange={handleUsernameChange} className="w-72 h-10" />
-            <Input type="password" placeholder="Password" name="password" required={true} value={password} onChange={handlePasswordChange} className="w-72 h-10" />
+            <div className='flex gap-3'>
+                <Input type={passwordVisible ? "text": "password"} placeholder="Password" name="password" required={true} value={password} onChange={handlePasswordChange} className="w-64 h-10" />
+                {
+                    passwordVisible ? (
+                        <EyeOff className='mt-2 cursor-pointer' onClick={togglePasswordVisible} />
+                    ):(
+                        <Eye className='mt-2 cursor-pointer' onClick={togglePasswordVisible} />
+                    )
+                }
+
+            </div>
             <h2>Already have an account? <Link to='/login'><span className='text-red-900 font-semibold'>Login</span></Link> </h2>
             <Button text='Signup' variant='secondary' onClick={handleSignup} />
+        </div>
+        <div>
+            {
+                error && <p className='font-semibold'>{error}</p>
+            }
         </div>
     </div>
   )
